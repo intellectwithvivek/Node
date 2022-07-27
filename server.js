4751911d9356b5794339145d9600b0785d201961
intellectwithvivek <intellectwithvivek@gmail.com>
2022-07-27 16:43:33 +0530

    removed unused codes

--- server.js
+++ server.js
@@ -41,34 +41,6 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 const customCss = fs.readFileSync(process.cwd() + "/swagger.css", "utf8");
 
-// place holder for the data
-let tasks = [
-  {
-    id: 1,
-    task: "task1",
-    assignee: "assignee1000",
-    status: "completed",
-  },
-  {
-    id: 2,
-    task: "task2",
-    assignee: "assignee1001",
-    status: "completed",
-  },
-  {
-    id: 3,
-    task: "task3",
-    assignee: "assignee1002",
-    status: "completed",
-  },
-  {
-    id: 4,
-    task: "task4",
-    assignee: "assignee1000",
-    status: "completed",
-  },
-];
-
 app.use(bodyParser.json());
 app.use(
   "/",
